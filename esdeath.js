b3238a4a6c6511d07bc2b8df324c287ed8749288
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-26 22:18:58 +0200

    'say' fix

--- esdeath.js
+++ esdeath.js
@@ -1,63 +1,46 @@
-const Discord = require('Discord.js')
-const fs = require('fs')
-const Enmap = require('enmap')
-const client = new Discord.Client()
-require('dotenv-flow').config()
+const Discord = require('Discord.js');
+const fs = require('fs');
+const Enmap = require('enmap');
+const client = new Discord.Client();
+require('dotenv-flow').config();
 
 const config = {
     token: process.env.token,
     prefix: process.env.prefix
 }
 
-const prefix = config.prefix
+const prefix = config.prefix;
 
-client.commands = new Enmap()
+client.commands = new Enmap();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`);
 
-    client.user.setActivity("YouTube", {type: "WATCHING"})
-})
+    client.user.setActivity("YouTube", {type: "WATCHING"});
+});
 
-client.on('message', (message) => {
+client.on('message', message => {
     if (message.author.bot) return;
     if (message.content.indexOf(prefix) !== 0) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ + /g)
-    const command = args.shift().toLowerCase()
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    const cmd = client.commands.get(command)
-    if(!cmd) return;
+    const cmd = client.commands.get(command);
+    if (!cmd) return;
 
-    cmd.run(client, message, args)
-
-    /*console.log(args)
-
-    switch (command) {
-        case 'ping': {
-            message.channel.send('pong!');
-            break;
-        }
-        case 'say': {
-            const response = args.join(' ');
-            message.channel.send(response);
-            break;
-        }
-        default:
-            message.channel.send('Command not found')
-            break;
-    }*/
-})
+    cmd.run(client, message, args);
+});
 
 fs.readdir('./Esdeath.Core/commands/', async (err, files) => {
     if(err) return console.error();
     files.forEach(file => {
-        if(!file.endsWith('.js')) return
-        let props = require(`./Esdeath.Core/commands/${file}`)
+        if(!file.endsWith('.js')) return;
+        let props = require(`./Esdeath.Core/commands/${file}`);
         let cmdName = file.split('.')[0];
-        console.log(`Loaded command ${cmdName}.`)
-        client.commands.set(cmdName, props)
+        console.log(`Loaded command ${cmdName}.`);
+        client.commands.set(cmdName, props);
     })
 })
 
-client.login(config.token)
+client.login(config.token);
