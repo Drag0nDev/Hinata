758220b644c2e0b465ef24a586015dd22d2e035d
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-28 00:57:33 +0200

    added command logging

--- Esdeath.Core/events/message.js
+++ Esdeath.Core/events/message.js
@@ -6,6 +6,8 @@ module.exports = (bot, message) => {
     if (message.author.bot) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
 
+    console.log(`\nCommand: '${message.content}' \nUser: '${message.author.tag}' \nServer: '${message.guild.name}' \nChannel: '${message.channel.name}'`);
+
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
