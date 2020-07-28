230f06a08c6e427f450a2cdea4337e6d5994f59e
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-28 11:55:38 +0200

    small add to the say command

--- Esdeath.Core/commands/say.js
+++ Esdeath.Core/commands/say.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args) => {
     const response = args.join(' ');
-    message.channel.send(response);
+    if (response === 'my creator is big gay')
+        message.channel.send(`No u ${message.author}`);
+    else
+        message.channel.send(response);
 }
 
 exports.help = {
