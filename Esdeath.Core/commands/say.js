8859e68ddc409dd70e5dff889d9efb9815751fc6
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-28 12:02:43 +0200

    fuck you aakash

--- Esdeath.Core/commands/say.js
+++ Esdeath.Core/commands/say.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
     const response = args.join(' ');
-    if (response === 'my creator is big gay')
+    if (response === 'my creator is big gay' || response === 'my creator is gay')
         message.channel.send(`No u ${message.author}`);
     else
         message.channel.send(response);
