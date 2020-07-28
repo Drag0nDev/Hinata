b96e77867992b9c3cfc12eb6eca7177dc729da01
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-28 21:10:04 +0200

    small change

--- Esdeath.Core/events/guildMemberAdd.js
+++ Esdeath.Core/events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = (bot, member) => {
-    let userLogs = member.guild.channels.find(c => c.name === 'member-logs');
+    let userLogs = member.guild.channels.find(c => c.name === 'member-log');
 
     userLogs.send(`${member.user.tag} has joined **${member.guild}**`);
 };
