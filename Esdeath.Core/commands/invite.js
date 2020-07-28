c8bb01eadcb08bbdfe247c842ce2cb592aa6c617
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-28 21:08:20 +0200

    added invite link commandadded invite command

--- Esdeath.Core/commands/invite.js
+++ Esdeath.Core/commands/invite.js
@@ -1,4 +1,4 @@
-exports.run = (client, message) => {
+exports.run = (client, message, args) => {
     message.channel.send('https://discord.com/api/oauth2/authorize?client_id=734814455833297037&permissions=2081418487&redirect_uri=https%3A%2F%2Fdiscordapp.com%2Foauth2%2Fauthorize%3F%26client_id%3D734814455833297037%26scope%3Dbot&scope=bot').catch(console.error);
 }
 
