2d4cfafd7da2bd037674abf4f7ffe45d72f165e5
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-29 19:32:32 +0200

    added setavatar command (bot owner only)

---
+++ Esdeath.Core/commands/setavatar.js
@@ -0,0 +1,15 @@
+const config = {
+    owner: process.env.OWNER
+};
+
+exports.run = (bot, message, args) => {
+    if(message.member.id === config.owner.toString()){
+        bot.user.setAvatar(args.toString());
+    }else{
+        message.channel.send(`${message.author} this is a command only for my creator!`);
+    }
+}
+
+exports.help = {
+    name: 'setAvatar'
+}
