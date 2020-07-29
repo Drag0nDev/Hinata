ee469d93645971112b3ce7eec038fb9e4f37e846
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-07-29 16:51:11 +0200

    fix

--- Esdeath.Core/commands/say.js
+++ Esdeath.Core/commands/say.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
     const response = args.join(' ');
-    if ((response.contains('gay').toLowerCase() || response.contains('gae').toLowerCase())
-        && (response.contains('master').toLowerCase() || response.contains('owner').toLowerCase()))
+    if ((response.includes('gay').toLowerCase() || response.includes('gae').toLowerCase())
+        && (response.includes('master').toLowerCase() || response.includes('owner').toLowerCase()))
         message.channel.send(`No u ${message.author}`);
     else
         message.channel.send(response);
