3e440d8a33e560bba5ce9e28bb668efbcae5147e
Lenn Van Haverbeke <lennvh046@gmail.com>
2020-09-27 22:37:16 +0200

    fix to commands

--- Esdeath.Core/commands/bonk.js
+++ Esdeath.Core/commands/bonk.js
@@ -2,7 +2,8 @@ const config = require("../../config.json");
 
 exports.run = (client, message, args) => {
     if(message.mentions.members.first()) {
-        if(args[0] === config.OWNER){
+        console.log(args[0]);
+        if(args[0] === `<@!${config.OWNER}>`){
             message.channel.send(`${message.author} <a:bonk:735549944814895115>, don't bonk my master!`);
         } else {
             message.channel.send(`${args[0]} <a:bonk:735549944814895115>`);
@@ -10,7 +11,6 @@ exports.run = (client, message, args) => {
     } else {
         message.channel.send('Please mention a user!');
     }
-
 }
 
 exports.help = {
--- Esdeath.Core/commands/dragon.js
+++ Esdeath.Core/commands/dragon.js
@@ -2,7 +2,7 @@ const config = require("../../config.json");
 
 exports.run = (client, message, args) => {
     if(message.member.id === config.OWNER) {
-        message.channel.send(`Hello master.\nYou are the best <@${config.owner.toString()}>!!! <:heart_diamond:738026632891334677>`);
+        message.channel.send(`Hello master.\nYou are the best <@${config.OWNER}>!!! <:heart_diamond:738026632891334677>`);
         return;
     }
 
