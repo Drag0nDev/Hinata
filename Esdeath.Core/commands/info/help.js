fc82c6779dc2715486054193390f9bd68f9858be
Drag0n <lennvh046@gmail.com>
2020-10-02 12:43:04 +0200

    small display change

--- Esdeath.Core/commands/info/help.js
+++ Esdeath.Core/commands/info/help.js
@@ -8,14 +8,14 @@ module.exports = {
     description: 'A function to show all commands',
     usage: '[command | alias]',
     run: (bot, message, args) => {
-        if(args[0])
+        if (args[0])
             return getCmd(bot, message, args[0]);
         else
             return getAll(bot, message);
     }
 }
 
-function getAll(bot, message){
+function getAll(bot, message) {
     const commands = (category) => {
         return bot.commands
             .filter(cmd => cmd.category === category)
@@ -33,17 +33,17 @@ function getAll(bot, message){
         .setFooter(`For more help type esdeath help [command]`);
 
     bot.categories.forEach(cat => {
-        if(message.member.id !== config.OWNER && cat === 'owner')
+        if (message.member.id !== config.OWNER && cat === 'owner')
             return;
         const _name = cat;
         const _value = commands(cat);
-        embed.addField(_name, _value, true);
+        embed.addField(`__${_name}__`, `${_value}`, true);
     });
 
     return message.channel.send(embed);
 }
 
-function getCmd(bot, message, input){
+function getCmd(bot, message, input) {
     const embed = new MessageEmbed();
     //info string default is the error message
     let info = `No information is found for command **${input.toLowerCase()}**`;
@@ -53,7 +53,7 @@ function getCmd(bot, message, input){
     if (!cmd) cmd = bot.commands.get(bot.aliases.get(input.toLowerCase()));
 
     //check if command exists
-    if(!cmd){
+    if (!cmd) {
         embed.setColor('#ff0000')
             .setTitle('No command found')
             .setDescription(info)
@@ -66,15 +66,15 @@ function getCmd(bot, message, input){
     embed.setColor('#85C1E9').setTimestamp();
 
     //add all cmd info
-    if(cmd.name){
+    if (cmd.name) {
         embed.setTitle(cmd.name);
         embed.addField(`Command name:`, cmd.name, false);
     }
-    if(cmd.aliases)
+    if (cmd.aliases)
         embed.addField('Aliases:', cmd.aliases.map(a => `\`${a}\``).join(", "), false);
     if (cmd.description)
         embed.addField('Description:', cmd.description, false);
-    if(cmd.usage){
+    if (cmd.usage) {
         embed.addField('Usage:', cmd.usage, false);
         embed.setFooter(`Syntax: [] = required, <> = optional`);
     }
