93fc7a823ecae03b3943dfa5353e9c176d3e1ebe
Drag0n <lennvh046@gmail.com>
2020-10-06 12:01:23 +0200

    small fixes

--- Esdeath.Core/commands/info/ping.js
+++ Esdeath.Core/commands/info/ping.js
@@ -6,6 +6,6 @@ module.exports = {
     run: async (bot, message, args) => {
         let ping = Date.now() - message.createdTimestamp; //As date received is ALWAYS smaller than date created, this should be more correct, than message - now().
         if (ping < 0) ping *= -1; //Should be obsolete
-        await message.channel.send("Bot ping: `" + `${ping}` + " ms`"); //Formatted it a bit better
+        await message.channel.send( + `\`${ping}\``); //Formatted it a bit better
     }
 }
