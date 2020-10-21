b562eb3bfee84021a7cb309d22e03398664de3ee
Drag0n <lennvh046@gmail.com>
2020-10-21 21:04:57 +0200

    small change

--- Esdeath.Core/commands/owner/guilds.js
+++ Esdeath.Core/commands/owner/guilds.js
@@ -7,15 +7,14 @@ module.exports = {
     description: 'Displaying all joined guilds with their respective member count',
     usage: '[command | alias]',
     run: async (bot, message) => {
-        let embed = new MessageEmbed().setTitle('Guilds');
+        let embed = new MessageEmbed().setTitle('Guilds').setColor(bot.embedColors.normal);
 
         if (!(message.member.id === config.OWNER)) {
             embed.setColor(bot.embedColors.error)
                 .setDescription('This command is only for my owner!');
         } else {
             bot.guilds.cache.forEach(guild => {
-                embed.setColor(bot.embedColors.normal)
-                    .addField(guild.name, `Member count: ${guild.memberCount}` + '\n' +
+                embed.addField(guild.name, `Member count: ${guild.memberCount}` + '\n' +
                     `Server ID: ${guild.id}`, true);
             });
         }
