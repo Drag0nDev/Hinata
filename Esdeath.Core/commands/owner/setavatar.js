a03b2c3c6fd42298288289c5cda6b1583fdaddc9
Drag0n <lennvh046@gmail.com>
2020-10-23 16:46:47 +0200

    now shows errors in discord

--- Esdeath.Core/commands/owner/setavatar.js
+++ Esdeath.Core/commands/owner/setavatar.js
@@ -1,4 +1,6 @@
 const config = require("../../../config.json");
+const {MessageEmbed} = require('discord.js');
+const log4js = require("log4js");
 
 module.exports = {
     name: 'setavatar',
@@ -6,12 +8,28 @@ module.exports = {
     category: 'owner',
     description: 'Command to change Esdeaths profilepicture',
     usage: '[command | alias] [link new picture]',
-    run: async (bot, message, args) => {
-        if (message.member.id === config.OWNER) {
-            await bot.user.setAvatar(args.toString());
-            await message.channel.send('Avatar changed successfully!');
-        } else {
-            await message.channel.send(`${message.author} this is a command only for my creator!`);
-        }
+    run: (bot, message, args) => {
+        const logger = log4js.getLogger();
+        let embed = new MessageEmbed();
+
+        if (!(message.member.id === config.OWNER))
+            return message.channel.send(embed.setDescription(`${message.author} this is a command only for my creator!`)
+                .setColor(bot.embedColors.error));
+
+        bot.user.setAvatar(args.toString()).then(updated => {
+            embed.setTitle(`setavatar`)
+                .setColor(bot.embedColors.normal)
+                .setDescription('Avatar changed successfully to:')
+                .setImage(bot.user.avatarURL({dynamic: true, size: 4096}));
+
+                message.channel.send(embed);
+            }
+        ).catch(err => {
+            embed.setDescription(err.message.toString().replace("Invalid Form Body\n" +
+                "avatar:", ""))
+                .setColor(bot.embedColors.error);
+            message.channel.send(embed);
+        })
+
     }
 }
