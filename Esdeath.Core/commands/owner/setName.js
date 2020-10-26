94ec0bfdbb5384f712c89f81b4b5bfb34ece9bbc
Drag0n <lennvh046@gmail.com>
2020-10-26 18:49:39 +0100

    now logs when the bot changes name and confirmation is sent in an embed

--- Esdeath.Core/commands/owner/setName.js
+++ Esdeath.Core/commands/owner/setName.js
@@ -1,4 +1,6 @@
 const config = require("../../../config.json");
+const {MessageEmbed} = require('discord.js');
+const log4js = require("log4js");
 
 module.exports = {
     name: 'setname',
@@ -7,12 +9,26 @@ module.exports = {
     description: 'Change esdeaths displayname',
     usage: '[command | alias] [new name]',
     run: async (bot, message, args) => {
-        if (message.member.id === config.OWNER) {
-            const newName = args.join(' ');
-            await bot.user.setUsername(newName);
-            await message.channel.send('Username changed successfully!');
-        } else {
-            await message.channel.send(`${message.author} this is a command only for my creator!`);
-        }
+        const logger = log4js.getLogger();
+        let embed = new MessageEmbed();
+
+        if (!(message.member.id === config.OWNER))
+            return message.channel.send(embed.setDescription(`${message.author} this is a command only for my creator!`)
+                .setColor(bot.embedColors.error));
+
+        const newName = args.join(' ');
+
+        let oldName = bot.user.tag;
+
+        await bot.user.setUsername(newName);
+
+        embed.setColor(bot.embedColors.normal)
+            .setDescription('Username changed successfully!')
+            .addField('Old name', oldName)
+            .addField('New name', bot.user.tag);
+
+        await message.channel.send(embed);
+
+        logger.warn(`Name changed from '${oldName}' to '${bot.user.tag}'`)
     }
 }
