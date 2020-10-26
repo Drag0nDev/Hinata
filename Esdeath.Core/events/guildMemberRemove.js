6d34b8bb53bb917db210f59bb3a0a10d40ade520
Drag0n <lennvh046@gmail.com>
2020-10-26 18:29:49 +0100

    fixed a bug where if a user left the server and was also the last person kicked it would trigger a kick modlog

--- Esdeath.Core/events/guildMemberRemove.js
+++ Esdeath.Core/events/guildMemberRemove.js
@@ -1,23 +1,33 @@
 const logger = require("log4js").getLogger();
-const { MessageEmbed } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 
 
 module.exports = async (bot, member) => {
     let embed = new MessageEmbed().setTimestamp()
     const channel = bot.channels.cache.find(channel => channel.id === '763039768870649856');
 
+    //get current date timestamp
+    let date = new Date();
+
     const fetchedLogs = await member.guild.fetchAuditLogs({
         limit: 1,
         type: 'MEMBER_KICK',
-    }).catch(console.error);
+    }).catch(err => {
+            logger.error(err);
+        }
+    );
 
     const kickLog = fetchedLogs.entries.first();
 
-    if (!kickLog) return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
+    if (
+        !kickLog ||
+        kickLog.createdTimestamp !== date.getTime()
+    )
+        return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
 
-    const { executor, target, reason } = kickLog;
+    const {executor, target, reason} = kickLog;
 
-    if(executor.id === bot.user.id)
+    if (executor.id === bot.user.id)
         return;
 
     if (target.id === member.id) {
@@ -28,8 +38,12 @@ module.exports = async (bot, member) => {
                 `**Responsible moderator:** ${executor.tag}`)
             .setFooter(`ID: ${member.id}`);
     } else {
-        logger.debug('Failed to load the audit log!');
+        logger.warn('Failed to load the audit log!');
     }
 
     await channel.send(embed);
 };
+
+function GetTimestamp(date){
+    return ((date.getMonth() + 1) + '/' + (date.getDate()) + '/' + date.getFullYear() + " " + date.getHours() + ':' + ((date.getMinutes() < 10) ? ("0" + date.getMinutes()) : (date.getMinutes())) + ':' + ((date.getSeconds() < 10) ? ("0" + date.getSeconds()) : (date.getSeconds())));
+}
