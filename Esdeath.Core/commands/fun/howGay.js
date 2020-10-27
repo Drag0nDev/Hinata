9b07c04ca4d25244d880366d6dc04b02e442cbe9
Drag0n <lennvh046@gmail.com>
2020-10-27 10:27:04 +0100

    now shows a gif when gayrate is over 50%

--- Esdeath.Core/commands/fun/howGay.js
+++ Esdeath.Core/commands/fun/howGay.js
@@ -14,14 +14,28 @@ module.exports = {
 
         if (message.mentions.members.first()) {
             let member = message.mentions.members.first();
+            let gayrate = getRandomInt(100);
+
             let msg = await message.channel.send(embed);
             await delay(1250);
-            embed.setDescription(`${member.user.tag} is ${getRandomInt(100)}% gay!`);
+
+            embed.setDescription(`${member.user.tag} is ${gayrate}% gay!`);
+
+            if (gayrate > 50)
+                embed.setImage('https://media1.tenor.com/images/07ca40330ec6b96b50de2f7539ca718d/tenor.gif');
+
             await msg.edit(embed);
         } else {
             let msg = await message.channel.send(embed);
+            let gayrate = getRandomInt(100);
+
             await delay(1250);
-            embed.setDescription(`${message.author.tag} is ${getRandomInt(100)}% gay!`);
+
+            embed.setDescription(`${message.author.tag} is ${gayrate}% gay!`);
+
+            if (gayrate > 50)
+                embed.setImage('https://media1.tenor.com/images/07ca40330ec6b96b50de2f7539ca718d/tenor.gif');
+
             await msg.edit(embed);
         }
     }
