980d2522cf156dfea92261ffc547999426d99767
Drag0n <lennvh046@gmail.com>
2020-10-27 10:00:16 +0100

    now will display in an embed and has a gif when the simprate is over 50 fixed the bug that wouldnt edit the message

--- Esdeath.Core/commands/fun/simprate.js
+++ Esdeath.Core/commands/fun/simprate.js
@@ -1,4 +1,5 @@
 const delay = require("delay");
+const {MessageEmbed} = require('discord.js');
 
 module.exports = {
     name: 'simprate',
@@ -7,15 +8,42 @@ module.exports = {
     description: 'Get the simprate of a person',
     usage: '[command | alias] <user mention>',
     run: async (bot, message, args) => {
+        let embed = new MessageEmbed()
+            .setTitle('simprate')
+            .setColor(bot.embedColors.normal)
+            .setDescription('Calculating');
+
+        let msg = await message.channel.send(embed);
+
         if (message.mentions.members.first()) {
-            let arg = args;
-            let msg = await message.channel.send('Calculating');
+            let member = message.mentions.members.first();
+            let simprate = getRandomInt(100);
+
             await delay(1250);
-            await msg.edit(`${arg} is ${getRandomInt(100)}% simp!`);
+
+            let editEmbed = new MessageEmbed()
+                .setTitle('simprate')
+                .setColor(bot.embedColors.normal)
+                .setDescription(`${member.user.tag} is ${simprate}% simp!`);
+
+            if (simprate > 50)
+                editEmbed.setImage('https://media1.tenor.com/images/b5cfc5d13e8640543a528c5da6412e8e/tenor.gif');
+
+            await msg.edit(editEmbed);
         } else {
-            let msg = message.channel.send('counting...');
+            let simprate = getRandomInt(100);
+
+            let editEmbed = new MessageEmbed()
+                .setTitle('simprate')
+                .setColor(bot.embedColors.normal)
+                .setDescription(`${message.author.tag} is ${simprate}% simp!`);
+
+            if (simprate > 50)
+                editEmbed.setImage('https://media1.tenor.com/images/b5cfc5d13e8640543a528c5da6412e8e/tenor.gif');
+
             await delay(1250);
-            await msg.edit(`${message.author} is ${getRandomInt(100)}% simp!`);
+
+            await msg.edit(editEmbed);
         }
     }
 }
