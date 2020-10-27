6e1ae3e0f7b84c0c0351a99df12f62c0140c63dc
Drag0n <lennvh046@gmail.com>
2020-10-27 15:59:12 +0100

    added gifs of esdeath and added a command to display them

---
+++ Esdeath.Core/commands/Reactions/esdeath.js
@@ -0,0 +1,28 @@
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+    //<editor-fold defaultstate="collapsed" desc="userinfo help">
+    name: 'esdeath',
+    category: 'Reactions',
+    description: 'show a gif of esdeath',
+    usage: '[command | alias]',
+    //</editor-fold>
+    run: async (bot, message) => {
+        let embed = new MessageEmbed().setColor(bot.embedColors.normal);
+
+        embed.setImage(getGif(bot).toString())
+            .setFooter('Powered by lost hopes and dreams');
+
+        await message.channel.send(embed);
+    }
+}
+
+function getGif(bot) {
+    let number = getRandom(Object.keys(bot.reactions.esdeath).length);
+
+    return bot.reactions.esdeath[number];
+}
+
+function getRandom(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
