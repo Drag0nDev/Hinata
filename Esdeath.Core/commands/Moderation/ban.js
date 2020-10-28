7049a2b36739c16fecb07b3c3c383a0bebc9634e
Drag0n <lennvh046@gmail.com>
2020-10-28 13:19:45 +0100

    made the ban command with sending a dm to the banned user

--- Esdeath.Core/commands/Moderation/ban.js
+++ Esdeath.Core/commands/Moderation/ban.js
@@ -1,27 +1,28 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = {
-    name: 'kick',
+    name: 'ban',
     category: 'Moderation',
-    description: 'Kick a member from the server',
+    description: 'ban a member from the server',
     usage: '[command | alias] [Member mention/id] <reason>',
+    neededPermissions: ['BAN_MEMBERS'],
     run: async (bot, message, args) => {
         let reason;
-        let embed = new MessageEmbed().setTimestamp().setColor(bot.embedColors.kick).setTitle('User kicked');
+        let embed = new MessageEmbed().setTimestamp().setColor(bot.embedColors.ban).setTitle('User banned');
         let guild = message.guild;
 
         //check if there is an argument
         if (!args[0])
-            return message.channel.send('Please provide a user to kick!');
+            return message.channel.send('Please provide a user to ban!');
 
         //check member permissions
-        if (!message.member.hasPermission('KICK_MEMBERS')) {
-            return message.channel.send(`${message.author} you do not have the **kick** permission!`);
+        if (!message.member.hasPermission('BAN_MEMBERS')) {
+            return message.channel.send(`${message.author} you do not have the **ban** permission!`);
         }
 
         //check bot permissions
-        if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
-            return message.channel.send('I do not have the required permission to kick members!');
+        if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
+            return message.channel.send('I do not have the required permission to ban members!');
         }
 
         const member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
@@ -31,19 +32,19 @@ module.exports = {
             return message.channel.send("No member found with this id/name!");
         }
 
-        //check if member is kickable
-        if (!member.kickable) {
-            return message.channel.send(`I can't kick **${member.user.tag}** due to role hierarchy!`);
+        //check if member is banable
+        if (!member.bannable) {
+            return message.channel.send(`I can't ban **${member.user.tag}** due to role hierarchy!`);
         }
 
-        //check if it is self kick, or bot kick
+        //check if it is self ban, or bot ban
         if (member.user.id === message.author.id) {
-            return message.channel.send("You can't kick yourself");
+            return message.channel.send("You can't ban yourself");
         }
 
         args.shift();
 
-        //kick the member
+        //set reason
         if (!args[0]) {
             reason = 'No reason provided';
         } else {
@@ -51,11 +52,15 @@ module.exports = {
         }
 
         const dmChannel = await member.createDM()
-        await dmChannel.send(`You got kicked from **${guild.name}** with reason: **${reason}**!`);
+        await dmChannel.send(`You got banned from **${guild.name}** with reason: **${reason}**!`);
         await dmChannel.deleteDM();
+        
+        await member.ban({
+            days: 7,
+            reason: `${reason}`
+        });
 
-        await member.kick(`${reason}`);
-        message.channel.send(`**${member.user.tag}** got kicked for reason: **${reason}**`);
+        message.channel.send(`**${member.user.tag}** got banned for reason: **${reason}**`);
 
         embed.setDescription(`**Member:** ${member.user.tag}\n` +
                 `**Reason:** ${reason}\n` +
