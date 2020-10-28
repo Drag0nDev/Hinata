a05652431b2163cbfc37b4d32789eb8fa942a5f8
Drag0n <lennvh046@gmail.com>
2020-10-28 10:48:12 +0100

    will now show dates according to yyyy/MM/dd

--- Esdeath.Core/commands/info/serverInfo.js
+++ Esdeath.Core/commands/info/serverInfo.js
@@ -104,21 +104,25 @@ function getAfkChannel(guild){
     return afkChannel.name;
 }
 
-function getDate(timestamp){
+function getDate(timestamp) {
     let date = new Date(timestamp);
 
+    let months = date.getMonth() + 1;
+    let days = date.getUTCDate();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
     let ampm = hours >= 12 ? 'PM' : 'AM';
 
+    months = months < 10 ? '0' + months : months;
+    days = days < 10 ? '0' + days : days;
     hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
+    hours = hours ? hours : 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
     let time = hours + ':' + minutes + ':' + seconds + ' ' + ampm;
 
-    return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getUTCDate()}\n${time}`
+    return `${date.getFullYear()}/${months}/${days}\n${time}`;
 }
 //</editor-fold>
