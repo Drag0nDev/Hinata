0b0bbf5496e0e718482d44de13e6d926388cb73d
Drag0n <lennvh046@gmail.com>
2021-01-18 11:18:23 +0100

    addition of commands to reward users with currency or take away currency (bot owner only)

---
+++ Stella.Core/commands/ownerdb/addBalance.js
@@ -0,0 +1,54 @@
+const {MessageEmbed} = require('discord.js');
+const {User} = require('../../misc/dbObjects');
+const config = require("../../../config.json");
+
+module.exports = {
+    name: 'addbalance',
+    aliases: ['addbal', 'ab'],
+    category: 'ownerdb',
+    description: 'Add balance to a specified user',
+    usage: '[command | alias] <id/mention>',
+    run: async (bot, message, args) => {
+        let embed = new MessageEmbed();
+        const id = new RegExp('^[0-9]{17,}');
+        const bal = new RegExp('^[0-9]*');
+
+        if (message.author.id !== config.owner) {
+            tools.ownerOnly(bot, message.channel)
+            return;
+        }
+
+        if (!id.exec(args[0])) {
+            embed.setColor(bot.embedColors.error)
+                .setDescription('Please provide a valid memberid / mention');
+
+            return message.channel.send(embed);
+        }
+
+        if (bal.exec(args[1])[0] === '')
+            return message.channel.send(embed.setColor(bot.embedColors.error)
+                .setDescription('Please provide an amount to add!'))
+
+        await User.findOne({
+            where:
+                {
+                    userId: args[0]
+                }
+        }).then(user => {
+            user.balance += parseInt(args[1]);
+            user.save();
+
+            embed.setTitle(`Add balance`)
+                .setColor(bot.embedColors.normal)
+                .setDescription(`Balance successfully added!
+                The balance of **${user.userTag}** is now **${user.balance}${bot.currencyEmoji}**.`)
+                .setTimestamp();
+        }).catch(err => {
+            embed.setColor(bot.embedColors.error)
+                .setDescription(`No user with id **${args[0]}** found in the database`)
+                .setTimestamp();
+        });
+
+        await message.channel.send(embed);
+    }
+}
