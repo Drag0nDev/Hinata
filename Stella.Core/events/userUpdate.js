f2766b5d10f8694bc38e6589c9c5ee3a05c76a41
Drag0n <lennvh046@gmail.com>
2021-02-01 17:27:17 +0100

    will now post a log with the changed name to all servers that the user is in if the memberlog channel is set

--- Stella.Core/events/userUpdate.js
+++ Stella.Core/events/userUpdate.js
@@ -1,13 +1,15 @@
 const {User} = require('../misc/dbObjects');
 const logger = require("log4js").getLogger();
+const {MessageEmbed} = require('discord.js');
+const tools = require("../misc/tools");
 
 module.exports = async (bot, oldMember, newMember) => {
-    usernameUpdate(oldMember, newMember);
+    await usernameUpdate(bot, oldMember, newMember);
 }
 
-function usernameUpdate(oldMember, newMember) {
+async function usernameUpdate(bot, oldMember, newMember) {
     try {
-        User.findOne({
+        await User.findOne({
             where: {
                 userId: oldMember.id
             }
@@ -19,6 +21,22 @@ function usernameUpdate(oldMember, newMember) {
                 userTag: `${newMember.username}#${newMember.discriminator}`,
                 userId: newMember.id
             });
+        });
+
+        await bot.guilds.cache.forEach(guild => {
+            if (guild.members.cache.get(newMember.id)) {
+                let embed = new MessageEmbed().setTimestamp()
+                    .setColor(bot.embedColors.logChange)
+                    .setAuthor(`${newMember.username}#${newMember.discriminator}`,
+                        newMember.avatarURL({dynamic: true}),
+                        newMember.avatarURL({dynamic: true, size: 4096}))
+                    .setTitle('Username changed')
+                    .addField('Old username', oldMember.username)
+                    .addField('New username', newMember.username)
+                    .setFooter(`ID: ${newMember.id}`);
+
+                tools.memberLogGuild(newMember, guild, embed);
+            }
         });
     } catch (err) {
         logger.error(err);
