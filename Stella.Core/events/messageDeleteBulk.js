a6e81854bf72ed2527276c7e68484ba9b95f01d4
Drag0n <lennvh046@gmail.com>
2021-02-02 11:35:52 +0100

    bulk delete log

---
+++ Stella.Core/events/messageDeleteBulk.js
@@ -0,0 +1,29 @@
+const logger = require("log4js").getLogger();
+const {MessageEmbed} = require('discord.js');
+const tools = require("../misc/tools");
+
+module.exports = async (bot, messages) => {
+    try {
+        let embed = new MessageEmbed().setTimestamp()
+            .setColor(bot.embedColors.logRemove)
+            .setTitle(`${messages.size} messages purged in ${messages.first().channel.name}`)
+            .setFooter(`latest ${messages.size} shown`);
+        let description = '';
+
+        for (let i = 0; i < messages.size && i < 50; i++) {
+            const message = messages.values().next().value;
+
+            if (message.author.bot) return;
+            const author = message.author;
+
+            if (message.content !== '')
+                description += `**[${author.username}#${author.discriminator}]**: ${message.content}\n`;
+        }
+
+        embed.setDescription(description);
+
+        await tools.messageLog(messages.first().guild, embed);
+    } catch (err) {
+        logger.error(err);
+    }
+}
