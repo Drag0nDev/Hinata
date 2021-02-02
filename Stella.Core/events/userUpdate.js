e8c6797ad14ed3f1261736fdb5ec9f53b4fc5336
Drag0n <lennvh046@gmail.com>
2021-02-02 10:08:05 +0100

    show if the avatar is changed

--- Stella.Core/events/userUpdate.js
+++ Stella.Core/events/userUpdate.js
@@ -4,10 +4,7 @@ const {MessageEmbed} = require('discord.js');
 const tools = require("../misc/tools");
 
 module.exports = async (bot, oldMember, newMember) => {
-    await usernameUpdate(bot, oldMember, newMember);
-}
-
-async function usernameUpdate(bot, oldMember, newMember) {
+    //update the name in the database
     try {
         await User.findOne({
             where: {
@@ -25,6 +22,7 @@ async function usernameUpdate(bot, oldMember, newMember) {
 
         await bot.guilds.cache.forEach(guild => {
             if (guild.members.cache.get(newMember.id)) {
+                //post logs if the username updated
                 if (oldMember.username !== newMember.username) {
                     let embed = new MessageEmbed().setTimestamp()
                         .setColor(bot.embedColors.logChange)
@@ -38,6 +36,7 @@ async function usernameUpdate(bot, oldMember, newMember) {
 
                     tools.memberLogGuild(newMember, guild, embed);
                 }
+                //post logs if the discriminator updated
                 if (oldMember.discriminator !== newMember.discriminator) {
                     let embed = new MessageEmbed().setTimestamp()
                         .setColor(bot.embedColors.logChange)
@@ -51,7 +50,22 @@ async function usernameUpdate(bot, oldMember, newMember) {
 
                     tools.memberLogGuild(newMember, guild, embed);
                 }
+                //post logs if the avatar updated
+                if (oldMember.avatar !== newMember.avatar) {
+                    let embed = new MessageEmbed().setTimestamp()
+                        .setColor(bot.embedColors.logChange)
+                        .setAuthor(`${newMember.username}#${newMember.discriminator}`,
+                            newMember.avatarURL({dynamic: true}),
+                            newMember.avatarURL({dynamic: true, size: 4096}))
+                        .setTitle('Avatar changed')
+                        .setImage(newMember.avatarURL({
+                            dynamic: true,
+                            size: 4096
+                        }))
+                        .setFooter(`ID: ${newMember.id}`);
 
+                    tools.memberLogGuild(newMember, guild, embed);
+                }
             }
         });
     } catch (err) {
