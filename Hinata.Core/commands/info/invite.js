83a0455bcf546d19200750c9ead733471b29bb72
Drag0n <lennvh046@gmail.com>
2021-02-16 19:19:04 +0100

    more detailed permissions then just administrator

--- Hinata.Core/commands/info/invite.js
+++ Hinata.Core/commands/info/invite.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 const logger = require("log4js").getLogger();
 
 module.exports = {
@@ -12,7 +12,11 @@ module.exports = {
         let embed = new MessageEmbed().setColor(bot.embedColors.normal);
 
         bot.generateInvite({
-            permissions: ["ADMINISTRATOR"]
+            permissions: [
+                "ADD_REACTIONS", "ATTACH_FILES", "BAN_MEMBERS", "CHANGE_NICKNAME", "EMBED_LINKS", "KICK_MEMBERS",
+                "MANAGE_CHANNELS", "MANAGE_GUILD", "MANAGE_EMOJIS", "MANAGE_MESSAGES", "MANAGE_WEBHOOKS", "CREATE_INSTANT_INVITE",
+                "SEND_MESSAGES", "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS", "VIEW_AUDIT_LOG", "VIEW_CHANNEL"
+            ]
         }).then(link => {
             embed.setDescription(`Invite me to your server: [link](${link}).`);
             message.channel.send(embed);
