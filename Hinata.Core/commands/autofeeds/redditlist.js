9126279103f7ec7607843f83555574a05de52052
Drag0nDev <lennvh046@gmail.com>
2021-03-15 12:54:29 +0100

    command to see all the subreddits feeds that are active in the server

---
+++ Hinata.Core/commands/autofeeds/redditlist.js
@@ -0,0 +1,33 @@
+const {MessageEmbed} = require("discord.js");
+const {Autofeeds} = require('../../misc/dbObjects');
+
+module.exports = {
+    name: 'redditlist',
+    aliases: ['rl'],
+    category: 'autofeeds',
+    description: 'Show a list of all followed subreddits',
+    usage: '[command | alias]',
+    run: async (bot, message) => {
+        const list = {
+            send: async (embed) => {
+                return message.channel.send(embed);
+            },
+            embed: new MessageEmbed().setTitle('Reddit list')
+                .setTimestamp(),
+            autofeeds: await Autofeeds.findAll({
+                where: {
+                    serverId: message.guild.id
+                }
+            }),
+            list: 'A list of all subreddits followed:\n\n'
+        }
+
+        list.autofeeds.forEach(autofeed => {
+            list.list += `**${autofeed.subreddit}**\n`
+        });
+
+        list.embed.setDescription(list.list);
+
+        await list.send(list.embed);
+    }
+}
