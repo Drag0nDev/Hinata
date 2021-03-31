b51543c961e809b7e45e2f81b7b1fd936ee492e2
Drag0nDev <lennvh046@gmail.com>
2021-03-31 10:26:08 +0200

    sends embeds into the support server to notify if the bot joins/leaves a server with name and membercount

--- Hinata.Core/events/guildCreate.js
+++ Hinata.Core/events/guildCreate.js
@@ -1,9 +1,36 @@
 const {Minor} = require('../misc/tools');
+const {MessageEmbed} = require('discord.js');
 
 module.exports = async (bot, server) => {
+    const post = {
+        support: await bot.guilds.cache.get('645047329141030936'),
+        embed: new MessageEmbed().setTitle('New server joined')
+            .setThumbnail(server.iconURL({
+                dynamic: true,
+                size: 4096
+            }))
+            .setColor(bot.embedColors.logs.join),
+        fields: [
+            {
+                name: 'Name:',
+                value: server.name,
+                inline: false
+            },
+            {
+                name: 'Member count:',
+                value: server.memberCount,
+                inline: false
+            }
+        ]
+    };
+    post.channel = await post.support.channels.cache.get('826725606619611187');
+
     for (const member of server.members.cache) {
         if (!member[1].user.bot) {
             await Minor.dbAdd(member, server);
         }
     }
+
+    post.embed.addFields(post.fields)
+    post.channel.send(post.embed);
 }
--- Hinata.Core/events/guildDelete.js
+++ Hinata.Core/events/guildDelete.js
@@ -1,6 +1,30 @@
 const {Server, Rewards, ServerSettings, Timers, ServerUser, Warnings} = require('../misc/dbObjects');
+const {MessageEmbed} = require('discord.js');
 
 module.exports = async (bot, server) => {
+    const post = {
+        support: await bot.guilds.cache.get('645047329141030936'),
+        embed: new MessageEmbed().setTitle('New server joined')
+            .setThumbnail(server.iconURL({
+                dynamic: true,
+                size: 4096
+            }))
+            .setColor(bot.embedColors.logs.join),
+        fields: [
+            {
+                name: 'Name:',
+                value: server.name,
+                inline: false
+            },
+            {
+                name: 'Member count:',
+                value: server.memberCount,
+                inline: false
+            }
+        ]
+    };
+    post.channel = await post.support.channels.cache.get('826725616560766996');
+
     await Server.destroy({
         where: {
             serverId: server.id
@@ -36,4 +60,7 @@ module.exports = async (bot, server) => {
             guildId: server.id
         }
     });
+
+    post.embed.addFields(post.fields)
+    post.channel.send(post.embed);
 }
