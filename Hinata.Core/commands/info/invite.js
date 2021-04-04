21a76e35c1df61c29e6e917baccceda4f9405fe3
Drag0nDev <lennvh046@gmail.com>
2021-04-04 22:13:32 +0200

    change to the invite link permissions

--- Hinata.Core/commands/info/invite.js
+++ Hinata.Core/commands/info/invite.js
@@ -13,7 +13,21 @@ module.exports = {
         let embed = new MessageEmbed().setColor(bot.embedColors.embeds.normal);
 
         bot.generateInvite({
-            permissions: ["ADMINISTRATOR"]
+            permissions: [
+                //for everything
+                "ADMINISTRATOR",
+                //when the person cant give admin permissions or they dont want the bot to have it
+                "CREATE_INSTANT_INVITE",
+                "KICK_MEMBERS", "BAN_MEMBERS", "MUTE_MEMBERS",
+                "MANAGE_CHANNELS", "MANAGE_GUILD",
+                "ADD_REACTIONS",
+                "VIEW_AUDIT_LOG", "VIEW_CHANNEL",
+                "SEND_MESSAGES", "MANAGE_MESSAGES", "EMBED_LINKS", "ATTACH_FILES",
+                "READ_MESSAGE_HISTORY", "USE_EXTERNAL_EMOJIS",
+                "USE_VAD",
+                "CHANGE_NICKNAME",
+                "MANAGE_NICKNAMES", "MANAGE_ROLES", "MANAGE_WEBHOOKS", "MANAGE_EMOJIS"
+            ]
         }).then(link => {
             embed.setDescription(`Invite me to your server: [link](${link}).`);
             message.channel.send(embed);
